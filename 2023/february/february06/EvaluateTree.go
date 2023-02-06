78b95ac2b15f3f9f50ce44bbac911ddfc46b6dd0
zhengpeng <847850277@qq.com>
2023-02-06 17:17:27 +0800

    计算布尔二叉树的值

---
+++ 2023/february/february06/EvaluateTree.go
@@ -0,0 +1,17 @@
+package february06
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func evaluateTree(root *TreeNode) bool {
+	if root.Left == nil {
+		return root.Val == 1
+	}
+	if root.Val == 2 {
+		return evaluateTree(root.Left) || evaluateTree(root.Right)
+	}
+	return evaluateTree(root.Left) && evaluateTree(root.Right)
+}
