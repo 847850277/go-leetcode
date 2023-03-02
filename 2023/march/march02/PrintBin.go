c09fd7f2b21e8103a9ab8160d1e227db8c80e49f
zhengpeng <847850277@qq.com>
2023-03-02 17:09:35 +0800

    二进制数转字符串

---
+++ 2023/march/march02/PrintBin.go
@@ -0,0 +1,18 @@
+package march02
+
+import "strings"
+
+func printBin(num float64) string {
+	sb := &strings.Builder{}
+	sb.WriteString("0.")
+	for sb.Len() <= 32 && num != 0 {
+		num = num * 2
+		digit := byte(num)
+		sb.WriteByte('0' + digit)
+		num = num - float64(digit)
+	}
+	if sb.Len() <= 32 {
+		return sb.String()
+	}
+	return "ERROR"
+}
